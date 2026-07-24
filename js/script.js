a1cf6a88074197db7d0508831e5e34f800e6d677
Navaniii1415 <navaniii2004@gmail.com>
2026-07-25 02:30:07 +0530

    Create JavaScript file

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+
+=========================================
+AYO - RUOK | Security
+Official Website Script
+Version : 1.0
+Author : Navaniii1415
+=========================================
+
+*/
+
+console.log("AYO - RUOK Website Loaded Successfully");
